e0cd61124d4f8d0146e82c5156d825292df06e86
EXCALIBUR <reisyusuf.1903@gmail.com>
2024-05-02 10:08:24 +0300

    yusuf

--- src/main/java/com/example/yenivesondeneme/controller/yusufcontroller.java
+++ src/main/java/com/example/yenivesondeneme/controller/yusufcontroller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/yusuf")
 public class yusufcontroller {
 
-    @GetMapping("/{list}")
+    @GetMapping()
     public String  listelle(){
         return "yusuf can";
     }
